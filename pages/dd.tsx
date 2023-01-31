c967cf995837ff614a5cc69ea5549ab1956307be
dongmin7208 <sayg3o3d@gmail.com>
2023-01-31 15:24:18 +0900

    ADD:CSS-modifier-transitions

--- pages/dd.tsx
+++ pages/dd.tsx
@@ -3,21 +3,27 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div className='grid min-h-screen gap-10 bg-slate-400 py-20 px-20'>
-      <div className='flex flex-col rounded-3xl bg-white p-6 shadow-xl'>
+      <div className='rounded-3xl bg-white p-6 shadow-xl'>
         <span className='text-2xl font-semibold'>Select Item</span>
         <div className='my-2 flex justify-between'>
-          <span className='text-gray-500'>Realforce</span>
-          <span className='font-semibold'>$190</span>
+          <span className='text-gray-500'>Grey Chair</span>
+          <span className='font-semibold'>$19</span>
         </div>
         <div className='flex justify-between'>
-          <span className='text-gray-500'>wood</span>
-          <span className='font-semibold'>$5</span>
+          <span className='text-gray-500'>Grey Chair</span>
+          <span className='font-semibold'>$19</span>
         </div>
         <div className='mt-2 flex justify-between border-t-2 border-dashed pt-2'>
           <span>Total</span>
-          <span className='font-semibold'>$195</span>
+          <span className='font-semibold'>$10</span>
         </div>
-        <button className='mx-auto mt-5 w-3/4 rounded-xl bg-blue-500 p-3 text-center text-white hover:bg-teal-500 hover:text-black focus:bg-red-500 active:bg-yellow-500'>
+        <button
+          className='mx-auto mt-5 w-3/4 rounded-xl
+          bg-blue-500 p-3 text-center text-white 
+          hover:bg-teal-500 hover:text-black
+          focus:bg-red-500 active:bg-yellow-500
+         '
+        >
           Checkout
         </button>
       </div>
@@ -28,18 +34,18 @@ const Home: NextPage = () => {
         <div className='relative -top-5 rounded-3xl bg-white p-6'>
           <div className='relative -top-16 flex items-end justify-between'>
             <div className='flex flex-col items-center'>
-              <span className='text-sm text-gray-500'>Orders</span>
+              <span className='text-xs text-gray-500'>Orders</span>
               <span className='font-medium'>340</span>
             </div>
-            <div className='h-24 w-24 rounded-full bg-red-400' />
+            <div className='h-24 w-24 rounded-full bg-zinc-300' />
             <div className='flex flex-col items-center'>
-              <span className='text-sm text-gray-500'>Spent</span>
+              <span className='text-xs text-gray-500'>Spent</span>
               <span className='font-medium'>$340</span>
             </div>
           </div>
-          <div className='relative -mt-14 -mb-5 flex flex-col items-center'>
+          <div className='relative  -mt-14 -mb-5 flex flex-col items-center'>
             <span className='text-lg font-medium'>Tony Molloy</span>
-            <span className='text-sm text-gray-500'>korea</span>
+            <span className='text-sm text-gray-500'>미국</span>
           </div>
         </div>
       </div>
@@ -57,24 +63,24 @@ const Home: NextPage = () => {
           <span className='text-xs text-gray-500'>Chair</span>
           <div className='mt-3 mb-5 flex items-center justify-between'>
             <div className='space-x-2'>
-              <button className='h-5 w-5 rounded-full bg-blue-500 ring-blue-500 ring-offset-2 transition focus:ring-2' />
-              <button className='h-5 w-5 rounded-full bg-red-500 ring-red-500 ring-offset-2 transition focus:ring-2' />
-              <button className='h-5 w-5 rounded-full bg-orange-500 ring-orange-500 ring-offset-2 transition focus:ring-2' />
+              <button className='h-5 w-5 rounded-full bg-yellow-500' />
+              <button className='h-5 w-5 rounded-full bg-indigo-500' />
+              <button className='h-5 w-5 rounded-full bg-teal-500' />
             </div>
             <div className='flex items-center space-x-5'>
-              <button className='flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500'>
+              <button className=' flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500'>
                 -
               </button>
               <span>1</span>
-              <button className='flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500'>
+              <button className=' flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500'>
                 +
               </button>
             </div>
           </div>
           <div className='flex items-center justify-between'>
             <span className='text-2xl font-medium'>$450</span>
-            <button className='rounded-lg bg-blue-500 py-2 px-6 text-center text-xs text-white'>
-              Add to Cart
+            <button className='rounded-lg bg-blue-500 py-2 px-8 text-center text-xs text-white'>
+              Add to cart
             </button>
           </div>
         </div>
