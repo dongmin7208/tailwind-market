18e4115549895706f2885c391b481f48a3ce19d2
dongmin7208 <sayg3o3d@gmail.com>
2023-01-31 10:28:21 +0900

    ADD:tailwind css content

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,28 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-red-500'>
-      <h1 className='text-black'>it works</h1>
+    <div className='grid gap-10 space-y-5 bg-slate-400 py-20 px-10'>
+      <div className='rounded-3xl bg-white p-6 shadow-xl'>
+        <span className='text-3xl font-semibold'>Select Item</span>
+        <div className='mb-2 flex justify-between'>
+          <span className='text-gray-500'>Grey Chair</span>
+          <span className='font-semibold'>$19</span>
+        </div>
+        <div className='flex justify-between'>
+          <span className='text-gray-500'>Tooly Table</span>
+          <span className='font-semibold'>$85</span>
+        </div>
+        <div className='mt-2 flex justify-between border-t-2 border-dashed pt-2'>
+          <span className=''>Total</span>
+          <span className='font-semibold'>$104</span>
+        </div>
+        <div className='mx-auto mt-5 w-2/4 rounded-xl bg-blue-500 p-2 text-center text-white'>
+          Checkout
+        </div>
+      </div>
+      <div className='rounded-2xl bg-white p-10 shadow-xl'></div>
+      <div className='rounded-2xl bg-white p-10 shadow-xl'></div>
+      <div className='rounded-2xl bg-white p-10 shadow-xl'></div>
     </div>
   );
 };
