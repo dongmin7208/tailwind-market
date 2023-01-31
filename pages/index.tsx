896a1da14b69f9d150bd272ca648dd49c2092649
dongmin7208 <sayg3o3d@gmail.com>
2023-01-31 11:56:06 +0900

    ADD:profile css

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,49 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className='grid gap-10 space-y-5 bg-slate-400 py-20 px-10'>
+    <div className='grid gap-10 bg-slate-400 px-20 py-20'>
       <div className='rounded-3xl bg-white p-6 shadow-xl'>
         <span className='text-3xl font-semibold'>Select Item</span>
-        <div className='mb-2 flex justify-between'>
-          <span className='text-gray-500'>Grey Chair</span>
-          <span className='font-semibold'>$19</span>
+        <div className='my-2 flex justify-between'>
+          <span className='text-gray-500'>Realforce</span>
+          <span className='font-semibold'>$190</span>
         </div>
         <div className='flex justify-between'>
-          <span className='text-gray-500'>Tooly Table</span>
-          <span className='font-semibold'>$85</span>
+          <span className='text-gray-500'>wood</span>
+          <span className='font-semibold'>$5</span>
         </div>
         <div className='mt-2 flex justify-between border-t-2 border-dashed pt-2'>
-          <span className=''>Total</span>
-          <span className='font-semibold'>$104</span>
+          <span>Total</span>
+          <span className='font-semibold'>$195</span>
         </div>
-        <div className='mx-auto mt-5 w-2/4 rounded-xl bg-blue-500 p-2 text-center text-white'>
+        <div className='mx-auto mt-5 w-2/4 rounded-xl bg-blue-500 p-3 text-center text-white'>
           Checkout
         </div>
       </div>
-      <div className='rounded-2xl bg-white p-10 shadow-xl'></div>
-      <div className='rounded-2xl bg-white p-10 shadow-xl'></div>
-      <div className='rounded-2xl bg-white p-10 shadow-xl'></div>
+      <div className='overflow-hidden rounded-3xl bg-white shadow-xl'>
+        <div className='bg-blue-500 p-6 pb-14'>
+          <span className='text-2xl text-white'>Profile</span>
+        </div>
+        <div className='relative -top-5 rounded-3xl bg-white p-6'>
+          <div className='relative -top-16 flex items-end justify-between'>
+            <div className='flex flex-col items-center'>
+              <span className='text-sm text-gray-500'>Orders</span>
+              <span className='font-medium'>340</span>
+            </div>
+            <div className='h-24 w-24 rounded-full bg-red-400' />
+            <div className='flex flex-col items-center'>
+              <span className='text-sm text-gray-500'>Spent</span>
+              <span className='font-medium'>$340</span>
+            </div>
+          </div>
+          <div className='relative -mt-10 -mb-5 flex flex-col items-center'>
+            <span className='text-lg font-medium'>Tony Molloy</span>
+            <span className='text-sm text-gray-500'>korea</span>
+          </div>
+        </div>
+      </div>
+      <div className='rounded-3xl bg-white p-6 shadow-xl'></div>
+      <div className='rounded-3xl bg-white p-6 shadow-xl'></div>
     </div>
   );
 };
