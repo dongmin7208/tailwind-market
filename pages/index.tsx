2a8d3b69b4d7ac96f496dd7154fdd4d7b882b6bd
dongmin7208 <sayg3o3d@gmail.com>
2023-02-04 14:51:13 +0900

    ADD: responsive color

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,42 +3,30 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div className='grid min-h-screen gap-10 bg-slate-400 py-20 px-20'>
-      <div className=' flex flex-col space-y-2 p-5'>
-        <input
-          type='file'
-          aria-label='label'
-          className='transition-colors file:cursor-pointer file:rounded-xl file:border-0 file:bg-purple-400 file:px-5 file:text-white file:hover:border file:hover:border-purple-400 file:hover:bg-white file:hover:text-purple-400'
-        />
-      </div>
-      <div className='flex flex-col rounded-3xl bg-white p-6 shadow-xl sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500'>
+      <div className='rounded-3xl bg-white p-6 shadow-xl sm:bg-red-400 sm:hover:bg-pink-800 md:bg-teal-400  lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500'>
         <span className='text-2xl font-semibold'>Select Item</span>
         <ul>
-          {[1, 2, 3, 4, 5].map((i) => (
-            // <div
-            //   key={i}
-            //   className='flex justify-between first:bg-blue-50 last:bg-blue-50 only:bg-red-500'
-            //  odd:bg-blue-50 even:bg-yellow-500<<<<<
-            // >
-            <div key={i} className='my-2 flex justify-between'>
-              <span className='text-gray-500'>wood</span>
-              <span className='font-semibold'>$5</span>
-            </div>
-          ))}
+          <div className='my-2 flex justify-between '>
+            <span className='text-gray-500'>Grey Chair</span>
+            <span className='font-semibold'>$19</span>
+          </div>
+          <div className='my-2 flex justify-between '>
+            <span className='text-gray-500'>red Chair</span>
+            <span className='font-semibold'>$19</span>
+          </div>
         </ul>
-        {['a', 'b', 'c', ''].map((c, i) => (
-          <li className='list-none bg-red-500 py-2 empty:hidden' key={i}>
-            {c}
-          </li>
-        ))}
-        <div className='my-2 flex justify-between'>
-          <span className='text-gray-500'>Realforce</span>
-          <span className='font-semibold'>$190</span>
-        </div>
+
         <div className='mt-2 flex justify-between border-t-2 border-dashed pt-2'>
           <span>Total</span>
-          <span className='font-semibold'>$195</span>
+          <span className='font-semibold'>$10</span>
         </div>
-        <button className='mx-auto mt-5 w-3/4 rounded-xl bg-blue-500 p-3 text-center text-white hover:bg-teal-500 hover:text-black focus:bg-red-500 active:bg-yellow-500'>
+        <button
+          className='mx-auto mt-5 block w-3/4
+          rounded-xl bg-blue-500 p-3 text-center text-white 
+          hover:bg-teal-500 hover:text-black
+          focus:bg-red-500 active:bg-yellow-500
+          '
+        >
           Checkout
         </button>
       </div>
@@ -49,17 +37,17 @@ const Home: NextPage = () => {
         <div className='relative -top-5 rounded-3xl bg-white p-6'>
           <div className='relative -top-16 flex items-end justify-between'>
             <div className='flex flex-col items-center'>
-              <span className='text-sm text-gray-500'>Orders</span>
+              <span className='text-xs text-gray-500'>Orders</span>
               <span className='font-medium'>340</span>
             </div>
-            <div className='h-24 w-24 rounded-full bg-red-400' />
+            <div className='h-24 w-24 rounded-full bg-zinc-300 transition-colors group-hover:bg-red-300' />
             <div className='flex flex-col items-center'>
-              <span className='text-sm text-gray-500'>Spent</span>
+              <span className='text-xs text-gray-500'>Spent</span>
               <span className='font-medium'>$340</span>
             </div>
           </div>
-          <div className='relative -mt-14 -mb-5 flex flex-col items-center'>
-            <span className='text-lg font-medium'>Park dongmin</span>
+          <div className='relative  -mt-14 -mb-5 flex flex-col items-center'>
+            <span className='text-lg font-medium'>dongmin park</span>
             <span className='text-sm text-gray-500'>korea</span>
           </div>
         </div>
@@ -74,37 +62,28 @@ const Home: NextPage = () => {
         </div>
         <div className='mb-5 h-72 bg-zinc-400' />
         <div className='flex flex-col'>
-          <span className='text-xl font-medium'>Swoon Lounge</span>
-          <span className='text-xs text-gray-500'>Chair</span>
+          <span className='text-xl font-medium'>share Lounge</span>
+          <span className='text-xs text-gray-500'>realforce</span>
           <div className='mt-3 mb-5 flex items-center justify-between'>
             <div className='space-x-2'>
-              <button
-                title='button'
-                className='h-5 w-5 rounded-full bg-blue-500 ring-blue-500 ring-offset-2 transition focus:ring-2'
-              />
-              <button
-                title='button'
-                className='h-5 w-5 rounded-full bg-red-500 ring-red-500 ring-offset-2 transition focus:ring-2'
-              />
-              <button
-                title='button'
-                className='h-5 w-5 rounded-full bg-orange-500 ring-orange-500 ring-offset-2 transition focus:ring-2'
-              />
+              <button className='h-5 w-5 rounded-full bg-yellow-500 ring-yellow-500 ring-offset-2 transition focus:ring-2' />
+              <button className='h-5 w-5 rounded-full bg-indigo-500 ring-indigo-500 ring-offset-2 transition focus:ring-2' />
+              <button className='h-5 w-5 rounded-full bg-teal-500 ring-teal-500 ring-offset-2 transition focus:ring-2' />
             </div>
             <div className='flex items-center space-x-5'>
-              <button className='flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500'>
+              <button className=' flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500'>
                 -
               </button>
               <span>1</span>
-              <button className='flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500'>
+              <button className=' flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500'>
                 +
               </button>
             </div>
           </div>
           <div className='flex items-center justify-between'>
             <span className='text-2xl font-medium'>$450</span>
-            <button className='rounded-lg bg-blue-500 py-2 px-6 text-center text-xs text-white'>
-              Add to Cart
+            <button className='rounded-lg bg-blue-500 py-2 px-8 text-center text-xs text-white'>
+              Add to cart
             </button>
           </div>
         </div>
@@ -114,3 +93,22 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+// <ul>
+//   {[1, 2, 3, 4, 5].map((i) => (
+//     // <div
+//     //   key={i}
+//     //   className='flex justify-between first:bg-blue-50 last:bg-blue-50 only:bg-red-500'
+//     //  odd:bg-blue-50 even:bg-yellow-500<<<<<
+//     // >
+//     <div key={i} className='my-2 flex justify-between'>
+//       <span className='text-gray-500'>wood</span>
+//       <span className='font-semibold'>$5</span>
+//     </div>
+//   ))}
+// </ul>
+// {['a', 'b', 'c', ''].map((c, i) => (
+//   <li className='list-none bg-red-500 py-2 empty:hidden' key={i}>
+//     {c}
+//   </li>
+// ))}
